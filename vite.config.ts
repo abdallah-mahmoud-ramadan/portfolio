db83bd26a5fb0bd02d262c3836bf438349e72343
Abdallah-Alqiran <abdallahalqiran765@gmial.com>
2026-06-25 13:35:56 +0300

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/web-portfolio/',
   plugins: [
     react({
       babel: {
