86684b0cacd40122c76adfb3f7548c1f9ce7424e
Abdallah-Alqiran <abdallahalqiran765@gmial.com>
2026-06-25 13:37:43 +0300

    change vite and main

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <Providers>
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
-          <BrowserRouter  basename="/web-portfolio">
+          <BrowserRouter  basename="/portfolio">
             <App />
           </BrowserRouter>
         </QueryClientProvider>
